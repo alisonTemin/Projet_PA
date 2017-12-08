7f573ea953ae26121d015fa9cc33fc66a5cd5979
Alison Temin <alison.temin@yahoo.fr>
2017-12-08 15:20:54 +0100

    Définition méthode dessinerRobot()

--- src/fr.unice.miage/PluginGraphisme.java
+++ src/fr.unice.miage/PluginGraphisme.java
@@ -1,17 +1,36 @@
 package fr.unice.miage;
 
+import javax.swing.*;
 import java.awt.*;
 
 class PluginGraphisme implements IGraphique{
 
-     Color colorRandom =
-     public void dessinerRobot(){
-        Graphics g;
+     Color colorRandom = new Color((int)(Math.random() * 0x1000000));
+     public void dessinerRobot() {
+         JFrame frame = new JFrame();
+         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+         frame.setVisible(true);
+         frame.setSize(600, 400);
+         JPanel panel = new JPanel() {
+             @Override
+             public void paintComponent(Graphics g) {
+                 super.paintComponent(g);
+                 g.setColor(colorRandom);
+                 g.fillRect(30, 30, 30, 30);
+             }
+         };
+         frame.add(panel);
+         frame.validate();
+         frame.repaint();
      }
 
      public void dessinerArme(){
-
+        
      }
 
+     public static void main(String[] args){
+         PluginGraphisme pg = new PluginGraphisme();
+         pg.dessinerRobot();
+     }
 
         }
