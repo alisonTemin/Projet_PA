1cc922ba1b8f56bd70aa74cf07f382ac7e5442c5
Alison Temin <alison.temin@yahoo.fr>
2017-12-12 16:53:02 +0100

    Modif ClassLoader

--- src/main/java/unice/miage/pa/Plugins/Graphism/Core/Graphism.java
+++ src/main/java/unice/miage/pa/Plugins/Graphism/Core/Graphism.java
@@ -3,12 +3,21 @@ package unice.miage.pa.Plugins.Graphism.Core;
 import unice.miage.pa.Elements.Robot;
 import unice.miage.pa.Plugins.Plugin;
 
+import javax.imageio.ImageIO;
 import javax.swing.*;
 import java.awt.*;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
 
 @Plugin(name="Graphism", required=1)
 public class Graphism implements IGraphism {
 
+    private JPanel panel;
+
+    public Graphism(JPanel panel){
+        this.panel = panel;
+    }
     private Color colorRandom = new Color((int)(Math.random() * 0x1000000));
 
     /**
@@ -17,7 +26,7 @@ public class Graphism implements IGraphism {
      */
     public void drawRobot(JPanel panel, final Robot robot) {
         // create our new robot
-        JPanel robotPanel = new JPanel() {
+       JPanel robotPanel = new JPanel() {
              @Override
              public void paintComponent(Graphics g) {
                  super.paintComponent(g);
@@ -32,7 +41,16 @@ public class Graphism implements IGraphism {
      }
 
      public void drawWeapon(){
-        
+         BufferedImage image = null;
+         try {
+             image = ImageIO.read(new File("images/images.jpg"));
+            ImageIcon myImage =  new ImageIcon(image);
+            panel.add(new JLabel(myImage));
+         } catch (IOException e) {
+         }
+
+
      }
+
 
 }
