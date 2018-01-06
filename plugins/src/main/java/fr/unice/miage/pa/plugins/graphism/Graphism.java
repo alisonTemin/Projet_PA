5c89c4a69a6a15112347bfcc00dd6520b6ba52d4
Léonard Cherouvrier <leonard@bigsool.com>
2018-01-06 21:18:42 +0100

    Code clean remove dead code

--- plugins/src/main/java/fr/unice/miage/pa/plugins/graphism/Graphism.java
+++ plugins/src/main/java/fr/unice/miage/pa/plugins/graphism/Graphism.java
@@ -2,17 +2,10 @@ package fr.unice.miage.pa.plugins.graphism;
 
 import fr.unice.miage.pa.plugins.Plugin;
 import fr.unice.miage.pa.plugins.PluginTrait;
-import fr.unice.miage.pa.plugins.attacks.weapons.Weapons;
 
-import javax.imageio.ImageIO;
 import javax.swing.*;
 
 import java.awt.*;
-import java.awt.geom.AffineTransform;
-import java.awt.image.AffineTransformOp;
-import java.awt.image.BufferedImage;
-import java.io.IOException;
-import java.io.InputStream;
 import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
 import java.util.Random;
@@ -61,90 +54,8 @@ public class Graphism {
         label.setLocation(x,label.getY());
     }
 
-    @PluginTrait(type="drawWeapon", on="robot")
-     public JLabel drawWeapon(Object robot, Weapons weapon, boolean isAtLeft) throws NoSuchMethodException, IllegalAccessException, InvocationTargetException {
-        int x = (Integer) this.callGetOnRobot("getX", robot);
-        int y = (Integer) this.callGetOnRobot("getY", robot);
-
-        int weaponX = x - 70;
-
-        if(isAtLeft){
-            weaponX = x + 70;
-        }
-        JLabel weaponLabel = null;
-
-        InputStream fileStream;
-         if (weapon.equals(Weapons.Sword)) {
-             fileStream = this.getResourceURL("sword.png");
-
-             try {
-                 weaponLabel = this.makeImageComponent(fileStream, weaponX, y, isAtLeft);
-                 this.panel.add(weaponLabel);
-             } catch (IOException e) {
-                 e.printStackTrace();
-             }
-         }
-         else if(weapon.equals(Weapons.Gun)){
-             fileStream = this.getResourceURL("gun.png");
-             try {
-                 weaponLabel = this.makeImageComponent(fileStream, weaponX, y, isAtLeft);
-                 this.panel.add(weaponLabel);
-             } catch (IOException e) {
-                 e.printStackTrace();
-             }
-
-         }
-         else if(weapon.equals(Weapons.MachineGun)){
-             fileStream = this.getResourceURL("machineGun.png");
-
-             try {
-                 weaponLabel = this.makeImageComponent(fileStream, weaponX, y, isAtLeft);
-                 this.panel.add(weaponLabel);
-             } catch (IOException e) {
-                 e.printStackTrace();
-             }
-
-         }
-
-         return weaponLabel;
-     }
-
-     private JLabel makeImageComponent(InputStream input, int x, int y, boolean invert) throws IOException {
-        return this.makeImageComponent(input, x, y, 200, 200, invert);
-     }
-
-     private JLabel makeImageComponent(InputStream input, int x, int y, int width, int height, boolean invert) throws IOException {
-         BufferedImage image = ImageIO.read(input);
-
-         if(!invert){
-             AffineTransform tx = AffineTransform.getScaleInstance(-1, 1);
-             tx.translate(-image.getWidth(null), 0);
-             AffineTransformOp op = new AffineTransformOp(tx, AffineTransformOp.TYPE_NEAREST_NEIGHBOR);
-             image = op.filter(image, null);
-         }
-
-         ImageIcon weapon = new ImageIcon(image);
-         JLabel label = new JLabel(weapon);
-         label.setBounds(x, y, width, height);
-         return label;
-     }
-
-    /**
-     * @return URI
-     */
-     private InputStream getResourceURL(String resourceName){
-         ClassLoader classLoader = Thread.currentThread().getContextClassLoader();
-
-         return classLoader.getResourceAsStream(resourceName);
-     }
-
     private Object callGetOnRobot(String getterName, Object robot) throws NoSuchMethodException, InvocationTargetException, IllegalAccessException {
         Method getter = robot.getClass().getDeclaredMethod(getterName);
         return getter.invoke(robot);
-    }
-
-    private Object callSetOnRobot(String setterName, Object robot, Object param) throws NoSuchMethodException, InvocationTargetException, IllegalAccessException {
-        Method setter = robot.getClass().getDeclaredMethod(setterName, Object.class);
-        return setter.invoke(robot, param);
     }
 }
