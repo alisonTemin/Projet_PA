e1d578530cb22549c514b3b8bcbd776902ed0c59
Léonard Cherouvrier <leonard@bigsool.com>
2018-01-10 20:25:30 +0100

    add annot to clarify

--- plugins/src/main/java/fr/unice/miage/pa/plugins/core/movement/RandomMove.java
+++ plugins/src/main/java/fr/unice/miage/pa/plugins/core/movement/RandomMove.java
@@ -1,6 +1,7 @@
 package fr.unice.miage.pa.plugins.core.movement;
 
 import fr.unice.miage.pa.plugins.core.annotations.Plugin;
+import fr.unice.miage.pa.plugins.core.annotations.PluginOverridable;
 import fr.unice.miage.pa.plugins.core.annotations.PluginTrait;
 
 import java.util.Random;
@@ -9,11 +10,13 @@ import java.util.Random;
 public class RandomMove {
 
     @PluginTrait(type="move", on="robot")
+    @PluginOverridable(name="move", on="strategy")
     public int nextPlace(){
         Random generator = new Random();
         return generator.nextInt(30) + 1;
     }
     @PluginTrait(type="moveY", on="robot")
+    @PluginOverridable(name="moveY", on="strategy")
     public int nextPlaceY(){
         Random generator = new Random();
         return generator.nextInt(10) +1 ;
