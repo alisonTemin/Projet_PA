6b1d3e9688d1a7e0f363338a83fe42b7071e303f
Léonard Cherouvrier <leonard@bigsool.com>
2018-01-11 00:49:53 +0100

    Make scrollpane scroll automatically

--- plugins/src/main/java/fr/unice/miage/pa/plugins/core/graphism/Console.java
+++ plugins/src/main/java/fr/unice/miage/pa/plugins/core/graphism/Console.java
@@ -3,6 +3,7 @@ package fr.unice.miage.pa.plugins.core.graphism;
 import fr.unice.miage.pa.plugins.core.annotations.Plugin;
 
 import javax.swing.*;
+import javax.swing.text.DefaultCaret;
 import java.io.*;
 
 @Plugin(name = "Console", required = 1, type = "core")
@@ -10,13 +11,15 @@ public class Console extends JFrame {
 
     public Console() {
         setSize(400, 300);
-        JTextArea aTextArea = new JTextArea();
-        add("Center", new JScrollPane(aTextArea));
+        JTextArea textarea = new JTextArea();
+        DefaultCaret caret = (DefaultCaret)textarea.getCaret();
+        caret.setUpdatePolicy(DefaultCaret.ALWAYS_UPDATE);
+        add("Center", new JScrollPane(textarea));
         setVisible(true);
         setLocation(400, 0);
         setTitle("Console");
 
-        FilteredStream filteredStream = new FilteredStream(new ByteArrayOutputStream(), aTextArea, "a.log");
+        FilteredStream filteredStream = new FilteredStream(new ByteArrayOutputStream(), textarea, "a.log");
         PrintStream aPrintStream = new PrintStream(filteredStream);
 
         System.setOut(aPrintStream); // catches System.out messages
